5bb16b2b1c61f681a321249c2b2079fbb6656f90
Jim <jim.cegelski@mortgagecto.com>
2017-02-17 08:52:53 -0500

    Work done during 2/13/2017 session

---
+++ PokerKata/PokerKata/Poker.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace PokerKata
+{
+    public class Poker
+    {
+        public static bool IsStraight(List<int> hand)
+        {
+            hand = InitializeHand(hand);
+            var cardsInOrder = 0;
+            for(int i = 0; i < hand.Count-1; i++)
+            {
+                if (hand[i] == hand[i + 1] - 1)
+                {
+                    cardsInOrder++;
+                }
+            }
+            return cardsInOrder >= 4;
+        }
+
+        public static List<int> InitializeHand(List<int> hand)
+        {
+            for (int i = 0; i < hand.Count - 1; i++)
+            {
+                if (hand[i] == 14) hand[i] = 1;
+            }
+            hand.Sort();
+            return hand;
+        }
+
+        public static List<int> InitializeHand2(List<int> hand)
+        {
+            var newHand = new List<int>();
+            foreach(int card in hand)
+            {
+                if (card == 14) newHand.Add(1);
+                newHand.Add(card);
+            }
+            newHand.Sort();
+            return newHand;
+        }
+    }
+}
---
+++ PokerKata/PokerTests/IsStraightShould.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokerKata;
+using System.Collections.Generic;
+
+namespace PokerTests
+{
+    [TestClass]
+    public class IsStraightShould
+    {
+        [TestMethod]
+        public void ReturnTrueWhenGivenFiveNumbersAllInRow()
+        {
+            var hand = new List<int>() { 9, 10, 11, 12, 13 };
+            Assert.IsTrue(Poker.IsStraight(hand));
+        }
+
+        [TestMethod]
+        public void ReturnFalseWhenGivenFiveNumbersNotInRow()
+        {
+            var hand = new List<int>() { 7, 10, 11, 12, 13 };
+            Assert.IsFalse(Poker.IsStraight(hand));
+        }
+
+        [TestMethod]
+        public void ReturnTrueWhenGivenFiveNumbersInRowWithAce()
+        {
+            var hand = new List<int>() {14, 2, 3, 4, 5 };
+            Assert.IsTrue(Poker.IsStraight(hand));
+        }
+        
+        [TestMethod]
+        public void ReturnTrueWhenGivenNumbersAreNotInOrder()
+        {
+            var hand = new List<int>() { 2,7,8,5,10,9,11 };
+            Assert.IsTrue(Poker.IsStraight(hand));
+        }
+
+        [TestMethod]
+        public void ReturnTrueForAHighStraight()
+        {
+            var hand = new List<int>() { 14, 13, 12, 11, 10 };
+            Assert.IsTrue(Poker.IsStraight(hand));
+        }
+
+    }
+}
